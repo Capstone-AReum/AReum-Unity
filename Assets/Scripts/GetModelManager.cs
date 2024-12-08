b1ceb5dc5d110e6a411b9144eeef207b190a19fa
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2024-12-08 23:54:22 +0900

    fix: Upload3, Main page Directional Light 설정 변경

--- Assets/Scripts/GetModelManager.cs
+++ Assets/Scripts/GetModelManager.cs
@@ -15,7 +15,7 @@ public class GetModelManager : MonoBehaviour
     public TextMeshProUGUI popupText;    // 팝업 내부 TextMeshPro
     public Button okButton;         // 팝업의 확인 버튼
 
-    private string postThumbnailUrl = "http://ec2-43-200-16-231.ap-northeast-2.compute.amazonaws.com/albums/thumbnails";
+    ///private string postThumbnailUrl = "http://ec2-43-200-16-231.ap-northeast-2.compute.amazonaws.com/albums/thumbnails";
     public static string glbUrl; // GLB 파일 URL을 저장
 
     void Start()
@@ -23,12 +23,12 @@ public class GetModelManager : MonoBehaviour
         popupPanel.SetActive(false);
 
         Debug.Log("SF3D API 호출 시작");
-        ///glbUrl = "https://cap-areum.s3.ap-northeast-2.amazonaws.com/models/tmp_d0ju8ct.glb";
-        ///SceneManager.LoadScene("Upload3");
-        StartCoroutine(SendModelRequest());
+        glbUrl = "https://cap-areum.s3.ap-northeast-2.amazonaws.com/models/tmpvx8y5_yk.glb";
+        SceneManager.LoadScene("Upload3");
+        ///StartCoroutine(SendModelRequest());
     }
 
-    IEnumerator SendModelRequest()
+    /*IEnumerator SendModelRequest()
     {
         string queryParam = "?source_id=" + DisplayGallery.selectedPhotoId;
         string url = postThumbnailUrl + queryParam;
@@ -58,5 +58,5 @@ public class GetModelManager : MonoBehaviour
             popupText.text = $"3D 모델 생성에 실패했습니다.\n{request.error}";
             okButton.gameObject.SetActive(true);
         }
-    }
+    }*/
 }
